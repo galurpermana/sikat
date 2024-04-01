ad90dfb00751e3d0003cea962678cfecd8eebd51
ngestack-dev <149769385+ngestack-dev@users.noreply.github.com>
2024-04-01 22:44:20 +0800

    Add files via upload

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,72 @@
+<div class="fixed top-32">
+    <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" aria-controls="sidebar" type="button"
+        class=""><img src="img/sidebar.svg" class="size-8 m-1 md:hidden" alt="">
+    </button>
+</div>
+
+<aside id="sidebar"
+    class="z-40 fixed top-32 left-0 w-64 h-screen transition-transform -translate-x-full md:translate-x-0"
+    aria-label="Sidebar">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <ul class="space-y-2 font-medium">
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/home-black.svg" class="size-8" alt="">
+                    <span class="ms-3 text-xl font-bold">Home</span>
+                </a>
+            </li>
+            <li class="text-center">Menu</li>
+            <li>
+                <a href="/riwayat"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/file-plus.svg" alt="">
+                    <span class="ms-3">Riwayat Pendaftaran</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/check-square.svg" alt="">
+                    <span class="ms-3">Konfirmasi Pendaftaran</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/upload.svg" alt="">
+                    <span class="ms-3">Unggah Berkas</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/file-text.svg" alt="">
+                    <span class="ms-3">Laporan Kegiatan</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/users.svg" class="" alt="">
+                    <span class="ms-3">Pembimbing Lapangan</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/calendar.svg" alt="">
+                    <span class="ms-3">Jadwal Seminar</span>
+                </a>
+            </li>
+            <li>
+                <a href="#"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <img src="img/check-circle.svg" alt="">
+                    <span class="ms-3">Lembar Pengesahan</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+</aside>
